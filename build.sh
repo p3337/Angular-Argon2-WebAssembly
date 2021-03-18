781b7b1825b970d8859a2373b00797d231b6bacf
peh <hansenpx@gmail.com>
2021-03-18 23:53:59 +0100

    Add missing definition [ARGON2_NO_THREADS] to ignore pthread api and update resulting argon2.js

--- build.sh
+++ build.sh
@@ -34,15 +34,19 @@ print "Compiling Argon2 WASM file..."
 emcmake cmake \
     -DCMAKE_VERBOSE_MAKEFILE=OFF \
     -DCMAKE_BUILD_TYPE=MinSizeRel \
-    -DCMAKE_C_FLAGS="-O3" \
+    -DCMAKE_C_FLAGS="-O3 -fPIC" \
     ../
 emmake make -j9
 
-print "\nEncoding binary WASM file to Base64 string..."
 
-echo -en "export const wasmBinaryBase64 =\"" > ./generated/argon2.ts
-base64 ./generated/argon2.wasm | tr -d "\n" >> ./generated/argon2.ts
-echo -en "\";\n" >> ./generated/argon2.ts
-rm ./generated/argon2.wasm
+sed -i 's/noExitRuntime=true;/&Module["free"]=_free;Module["malloc"]=_malloc;Module["argon2Verify"]=_argon2_verify;Module["argon2Hash"]=_argon2_hash;Module["argon2ErrorMessage"]=_argon2_error_message;/' ./generated/argon2.js
+ed -s ./generated/argon2.js <<< $'-5,$d\nwq'
+
+# print "\nEncoding binary WASM file to Base64 string..."
+
+# echo -en "export const wasmBinaryBase64 =\"" > ./generated/argon2.ts
+# base64 ./generated/argon2.wasm | tr -d "\n" >> ./generated/argon2.ts
+# echo -en "\";\n" >> ./generated/argon2.ts
+# rm ./generated/argon2.wasm
 
 print "Done."
